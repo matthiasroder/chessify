0590bca0df8317dc04a935facafb15166a8d88be
Matthias Röder <mr1977@gmail.com>
2023-01-24 07:19:12 +0100

    Added info and score_diff to df.

--- chessify.py
+++ chessify.py
@@ -2,6 +2,7 @@ import chess
 import chess.pgn
 import chess.engine
 import pandas as pd
+import time
 
 def open_games(file):
     games = []
@@ -27,15 +28,27 @@ def analyze_position(fen):
     global progress
     engine = chess.engine.SimpleEngine.popen_uci("stockfish")
     board = chess.Board(fen)
-    info = engine.analyse(board, chess.engine.Limit(time=0.01))
+    info = engine.analyse(board, chess.engine.Limit(depth=24))
     engine.quit()
     score = info['score'].white().score()
     progress += 1
     print(progress, fen, score)
     return(score)
 
-games = open_games('./data/lichess_matthiasroder_2023-01-21.pgn')
+def create_analysis(fen):
+    global progress
+    engine = chess.engine.SimpleEngine.popen_uci("stockfish")
+    board = chess.Board(fen)
+    info = engine.analyse(board, chess.engine.Limit(depth=24))
+    engine.quit()
+    progress += 1
+    print(progress, fen, info)
+    return(info)
+
+
+games = open_games('./data/lichess_licoach_2023-01-23.pgn')
 fens = create_fens(games)
+
 df = pd.DataFrame(games, columns=['pgn'])
 df['id'] = df['pgn'].apply(lambda x: x.headers['Site'].split('/')[-1:][0])
 df['url'] = df['pgn'].apply(lambda x: x.headers['Site'])
@@ -45,5 +58,9 @@ df['mainline_moves'] = df['pgn'].apply(lambda x: x.mainline_moves())
 df2 = pd.DataFrame([(tup.pgn, tup.id, tup.url, tup.is_white, tup.mainline, tup.mainline_moves, move) for tup in df.itertuples() for move in tup.mainline_moves])
 df2 = df2.rename(columns={0:'pgn',1:'id',2:'url',3:'is_white',4:'mainline',5:'mainline_moves',6:'move'})
 df2['fen'] = fens
+df['ply'] = df.groupby(['id']).cumcount()+1
 progress = 0
 df2['score'] = df2['fen'].apply(analyze_position)
+progress = 0
+df2['info'] = df2['fen'].apply(create_analysis)
+df2['score_diff'] = df2['score'].diff()
