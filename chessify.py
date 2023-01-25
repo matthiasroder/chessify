a5a9753ecf235fae105466b573ad2bace802c8c7
Matthias Röder <mr1977@gmail.com>
2023-01-25 08:52:06 +0100

    Added find positions

--- chessify.py
+++ chessify.py
@@ -3,6 +3,7 @@ import chess.pgn
 import chess.engine
 import pandas as pd
 import time
+import os
 
 def open_games(file):
     games = []
@@ -45,22 +46,28 @@ def create_analysis(fen):
     print(progress, fen, info)
     return(info)
 
+def find_positions(file, fen, outfile):
+    command = f"pgn-extract -o{outfile} --fenpattern '{fen}' {file}"
+    os.system(command)
+    print('Done')
 
-games = open_games('./data/lichess_licoach_2023-01-23.pgn')
-fens = create_fens(games)
+if __name__ == '__main__':
+    games = open_games('./data/lichess_licoach_2023-01-23.pgn')
+    fens = create_fens(games)
 
-df = pd.DataFrame(games, columns=['pgn'])
-df['id'] = df['pgn'].apply(lambda x: x.headers['Site'].split('/')[-1:][0])
-df['url'] = df['pgn'].apply(lambda x: x.headers['Site'])
-df['is_white'] = df['pgn'].apply(lambda x: x.headers['White'] == 'matthiasroder')
-df['mainline'] = df['pgn'].apply(lambda x: x.mainline())
-df['mainline_moves'] = df['pgn'].apply(lambda x: x.mainline_moves())
-df2 = pd.DataFrame([(tup.pgn, tup.id, tup.url, tup.is_white, tup.mainline, tup.mainline_moves, move) for tup in df.itertuples() for move in tup.mainline_moves])
-df2 = df2.rename(columns={0:'pgn',1:'id',2:'url',3:'is_white',4:'mainline',5:'mainline_moves',6:'move'})
-df2['fen'] = fens
-df['ply'] = df.groupby(['id']).cumcount()+1
-progress = 0
-df2['score'] = df2['fen'].apply(analyze_position)
-progress = 0
-df2['info'] = df2['fen'].apply(create_analysis)
-df2['score_diff'] = df2['score'].diff()
+    df = pd.DataFrame(games, columns=['pgn'])
+    df['id'] = df['pgn'].apply(lambda x: x.headers['Site'].split('/')[-1:][0])
+    df['url'] = df['pgn'].apply(lambda x: x.headers['Site'])
+    df['is_white'] = df['pgn'].apply(lambda x: x.headers['White'] == 'matthiasroder')
+    df['mainline'] = df['pgn'].apply(lambda x: x.mainline())
+    df['mainline_moves'] = df['pgn'].apply(lambda x: x.mainline_moves())
+    df2 = pd.DataFrame([(tup.pgn, tup.id, tup.url, tup.is_white, tup.mainline, tup.mainline_moves, move) for tup in df.itertuples() for move in tup.mainline_moves])
+    df2 = df2.rename(columns={0:'pgn',1:'id',2:'url',3:'is_white',4:'mainline',5:'mainline_moves',6:'move'})
+    df2['fen'] = fens
+    df['ply'] = df.groupby(['id']).cumcount()+1
+    progress = 0
+    df2['score'] = df2['fen'].apply(analyze_position)
+    progress = 0
+    df2['info'] = df2['fen'].apply(create_analysis)
+    df2['score_diff'] = df2['score'].diff()
+    print('Done.')
